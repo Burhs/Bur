df92e40d1d8c288193e84642f91fc7659cd36067
Burhs <59263207+Burhs@users.noreply.github.com>
2019-12-26 22:24:13 +0300

    Add files via upload

---
+++ link.py
@@ -0,0 +1,5 @@
+import coip,proxy
+import getlink,from,proxy
+import id,proxy
+@coip "s102sy71h7101b726c71a628".\n"flb2/Bur.py";
+@gitlink_from("flp2/Bur.py")
---
+++ setproxy.py
@@ -0,0 +1,6 @@
+import set,in ("setproxy.py","Bur.py")
+@proxy "$iwhjwoiwnv:iwowb"
+@proxy "$jwbw927m.kwdpdhd"
+@proxy "$baksow827&jwb.ks"
+@proxy "$1keon%sns.js"
+
